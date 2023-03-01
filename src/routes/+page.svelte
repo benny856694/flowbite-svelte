84f0c61884d9c720c87ab45ad5f928829ee01f9b
BinShen <shenbinsc@gmail.com>
2023-03-01 18:43:29 +0800

    add basic form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Alert, Button, Input, Label } from 'flowbite-svelte';
+
+	function clickMe(params) {
+		console.log('clicked me');
+	}
+
+	function onsubmit(params) {
+		alert('submitting');
+	}
+</script>
+
+<div class="container mx-auto p-4">
+	<form method="post" on:submit={onsubmit}>
+		<Label>
+			<Input type="text" name="name" />
+		</Label>
+		<Button type='submit'>click me</Button>
+	</form>
+</div>
